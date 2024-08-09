29eff93d6e30653f4b9e12199dbb45c9880474ad
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-08-09 08:06:07 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -10,6 +10,6 @@ class Cadastro:
     def __str__(self):
      return f'Seu nome:{self.nome}  Sua idade:{self.idade}  Seu sexo:{self.sexo}  Seu email:{self.email}  Seu telefone:{self.telefone}   Seu endereco:{self.endereco}'
  
-cad1 = Cadastro ('Matheus',17,'masc','Matheussantos@gmail.com','41 4321 1234','rua 15')
+cad1 = Cadastro ('Gabrielly',16,'fem','gaby0812silva@gmail.com','41 4922 0922','rua 16')
 
 print (cad1)
